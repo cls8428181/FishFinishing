015ecb18a704f47139be4795a78b83a45ef07575
常立山 <changls01@163.com>
2019-03-26 10:44:04 +0800

    添加工程

---
+++ FishFinishing/FishFinishing/Application/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FishFinishing
+//
+//  Created by 常立山 on 2019/3/26.
+//  Copyright © 2019 常立山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
